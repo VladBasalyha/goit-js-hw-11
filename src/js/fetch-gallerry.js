e06c97bf5ae6358cd19259a83ffa9f19dd3bb277
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-09-28 04:52:06 +0300

    added notifications for searching images

--- src/js/fetch-gallerry.js
+++ src/js/fetch-gallerry.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 const API_KEY = '30146257-64982587d71520e4d5095fa16';
 
 export default class PixabayImages {
@@ -15,9 +16,12 @@ export default class PixabayImages {
   getGalleryImages(name) {
     return axios
       .get(
-        `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+        `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=false&per_page=40`
       )
       .then(response => {
+        Notiflix.Notify.success(
+          `Hooray! We found ${response.data.totalHits} images.`
+        );
         return response.data.hits;
       });
   }
