d57c3af3964253130541759dc91bc8631ac23c05
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-09-30 11:55:25 +0300

    vdsavas

--- src/js/gallery-slider.js
+++ src/js/gallery-slider.js
@@ -4,7 +4,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 let lightbox = new SimpleLightbox('.photo-card a', {
   captions: true,
   captionsData: 'alt',
-  captionDelay: 250,
+  captionDelay: 200,
 });
 
 export { lightbox };
